2cf39d4aeacb49e3403784b3a5a66a8b9a6234de
Sallee <885331635@qq.com>
2024-12-23 22:43:11 +0800

    4.2 更新地图中心点

--- cvAutoTrack/src/utils/Utils.cpp
+++ cvAutoTrack/src/utils/Utils.cpp
@@ -281,7 +281,7 @@ namespace TianLi::Utils
     {
         int id = 0;
         cv::Point2d dstPoint = cv::Point2d(x, y);
-        cv::Point2i center = { 6067, 3662 };
+        cv::Point2i center = Resources::getInstance().map_relative_center;
         //先检查在哪个洞内
         for (auto& [key, value] : area_mappers)
         {
