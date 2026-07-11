01411243e338be76afbee2d66c18344b1b6e7caf
Sallee <885331635@qq.com>
2026-07-11 23:28:45 +0800

    build: 调整构建方式

---
+++ cmake/third_party.cmake
@@ -0,0 +1,4 @@
+set(OPENCV_STATIC_DIR  "C:/Packages/opencv-static"  CACHE PATH "OpenCV 静态库根目录")
+set(CURL_STATIC_DIR    "C:/Packages/curl-static"    CACHE PATH "libcurl 静态库根目录")
+set(OPENSSL_STATIC_DIR "C:/Packages/openssl-static" CACHE PATH "OpenSSL 静态库根目录")
+set(NLOHMANN_JSON_DIR  "C:/Packages/nlohmann_json"  CACHE PATH "nlohmann-json 头文件目录")
